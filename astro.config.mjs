2b905806e5a33a11a6825213fd7d6c27925ca3c9
jesusadrian14 <jesuscasierra928@gmail.com>
2026-05-09 17:50:59 -0500

    xdndc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
+import tailwindcss from "@tailwindcss/vite";
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	
+	vite: {
+		plugins: [tailwindcss()],
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, './src'),
+			},
+		},
+	},
+});
